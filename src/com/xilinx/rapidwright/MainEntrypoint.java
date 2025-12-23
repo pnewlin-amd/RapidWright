23e39d7a5184c02b62ed3085a8e2570192c6c230
Perry Newlin <perry.newlin@amd.com>
2025-12-23 16:36:44 -0500

    snakepath CLI does not meet requirements for multi-fpga comb-path detection (the type of snake paths we look for)

--- src/com/xilinx/rapidwright/MainEntrypoint.java
+++ src/com/xilinx/rapidwright/MainEntrypoint.java
@@ -71,7 +71,6 @@ import com.xilinx.rapidwright.examples.PrintEDIFInstances;
 import com.xilinx.rapidwright.examples.ReportTimingExample;
 import com.xilinx.rapidwright.examples.RunSATRouterExample;
 import com.xilinx.rapidwright.examples.SLRCrosserGenerator;
-import com.xilinx.rapidwright.examples.SnakePathCLI;
 import com.xilinx.rapidwright.examples.StampPlacement;
 import com.xilinx.rapidwright.examples.UpdateRoutingUsingSATRouter;
 import com.xilinx.rapidwright.examples.tilebrowser.PartTileBrowser;
@@ -218,7 +217,6 @@ public class MainEntrypoint {
         addFunction("RWRoute", RWRoute::main);
         addFunction("SLRCrosserGenerator", SLRCrosserGenerator::main);
         addFunction("SmallestEnclosingCircle", SmallestEnclosingCircle::main);
-        addFunction("SnakePath", SnakePathCLI::main);
         addFunction("StampPlacement", StampPlacement::main);
         addFunction("StandaloneEntrypoint", StandaloneEntrypoint::main);
         addFunction("StringTools", StringTools::main);
